35738bf91c2964545431fdd9cf3cb890b1a3555d
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-05-08 19:39:27 +0200

    Una sola fuente de verdad

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable react/prop-types */
-import { useState, useId } from "react";
+import { useId } from "react";
 import "./Filters.css";
 import { useFilters } from "../hooks/useFilters";
 
 const Filters = () => {
-  // Usamos un estado para mostrar el valor del rango de precio.
-  const [minPrice, setMinPrice] = useState(0);
-
   // Traemos 'setFilters' del Custom Hook directamente a este componente 'Filters' que es quien va a utilizarlo y eliminamos el Prop Drilling de App > Header > Filters.
-  const {setFilters} = useFilters();
+  // A su vez, traemos 'filters' y consumimos los valores del contexto para mostrarlos en el rango y así tener solo una fuente de la verdad eliminando el estado local que teníamos al principio.
+  const {filters, setFilters} = useFilters();
 
   // Usamos el Hook de React 'useID' para generar identificadores únicos y evitarnos posibles fallos o repeticiones en algún otro componente de la aplicación.
   const minPriceFilterId = useId();
@@ -16,7 +14,6 @@ const Filters = () => {
 
   // Funcion manejadora para el precio mínimo que irá en el onChange del input.
   const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value);
     setFilters(prevState => ({
       ...prevState,
       minPrice: event.target.value
@@ -42,8 +39,9 @@ const Filters = () => {
           min='0'
           max='1000'
           onChange={handleChangeMinPrice}
+          value={filters.minPrice}
         />
-        <span>${minPrice}</span>
+        <span>${filters.minPrice}</span>
       </div>
 
       <div>
